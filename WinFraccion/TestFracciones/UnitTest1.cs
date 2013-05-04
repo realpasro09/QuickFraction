e1e62dbf7d69c8c0a455232d59af5e2b78745b65
Rene A. Rosa <realpasro09@hotmail.com>
2013-05-03 20:38:29 -0600

    Creando el proyecto

---
+++ WinFraccion/TestFracciones/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WinFraccion;
+
+namespace TestFracciones
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var f = new Fraccion();
+            int numerador = f.Numerador;
+            int denominador = f.Denominador;
+            var b = (numerador == 1) && (denominador == 1);
+            Assert.AreEqual(true,b);
+        }
+    }
+}
---
+++ WinFraccion/WinFraccion/Fraccion.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinFraccion
+{
+    public class Fraccion
+    {
+        public int Numerador { get; set; }
+
+        public int Denominador { get; set; }
+    }
+}
